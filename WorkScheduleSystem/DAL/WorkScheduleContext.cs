f2717aa2de98838ed1454618da49d1ddcb622150
jwhitney5 <jwhitney5@studentmail.nait.ca>
2014-10-29 18:42:41 -0600

    Created Context Class (Finished Assignment #3)
    Created WorkScheduleContext class

---
+++ WorkScheduleSystem/DAL/WorkScheduleContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using WorkScheduleSystem.Entities;
+using System.Data.Entity;
+#endregion
+
+namespace WorkScheduleSystem.DAL
+{
+    public class WorkScheduleContext : DbContext
+    {
+        public WorkScheduleContext() : base("name=WorkSchedule") { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeSkills> EmployeeSkills { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<PlacementContract> PlacementContracts { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
